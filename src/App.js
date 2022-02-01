1ea8e1c138ea6c41731ac6d7a4929c44d26bcc9e
FORHADUL ISLAM <mdforhadul44@gmail.com>
2022-02-01 14:36:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import AbsentStudent from "./Components/AbsentStudent";
-import AddStudent from "./Components/AddStudent";
-import PresentStudent from "./Components/PresentStudent";
-import StudentList from "./Components/StudentList";
 
 function App() {
   const [studentList, setStudentList] = useState([]);
-  const [studentPresentList, setStudentPresentList] = useState([]);
-  const [studentAbsentList, setStudentAbsentList] = useState([]);
 
-  return (
-    <div className="App">
-      <AddStudent setStudentList={setStudentList} studentList={studentList} />
-      <div className="box__container">
-        <StudentList
-          studentList={studentList}
-          setStudentList={setStudentList}
-          studentPresentList={studentPresentList}
-          setStudentPresentList={setStudentPresentList}
-          studentAbsentList={studentAbsentList}
-          setStudentAbsentList={setStudentAbsentList}
-        />
-        <PresentStudent studentPresentList={studentPresentList} />
-        <AbsentStudent studentAbsentList={studentAbsentList} />
-        <div className="word__mening">
-          P-Present <br />
-          A-Absent <br />
-          M-Mistackly Add
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
